8d5fc5ef67cc697ceb8ea5e1d9b0f8c83f5a075f
msanvarov <salim.anvarov@yahoo.com>
2019-07-21 22:25:02 -0400

    config module for injecting environment variables

---
+++ src/modules/config/config.service.ts
@@ -0,0 +1,18 @@
+import * as dotenv from "dotenv";
+import * as fs from "fs";
+
+export class ConfigService {
+  private readonly envConfig: { [key: string]: string };
+
+  constructor(filePath: string) {
+    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+
+  isEnv(env: string) {
+    return this.envConfig.APP_ENV === env;
+  }
+}
