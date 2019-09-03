3b864aa6be50a7ad5e32dca04bcb51a207379d28
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 17:05:26 -0400

    changed default routes to use api prefix and created delete route for testing

--- src/modules/main/app.controller.ts
+++ src/modules/main/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Req, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
 import { AuthGuard } from "@nestjs/passport";
+import { ProfileService } from "../profile/profile.service";
 
 @Controller()
 @ApiBearerAuth()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly profileService: ProfileService,
+  ) {}
 
   @Get()
   @UseGuards(AuthGuard())
@@ -16,11 +20,18 @@ export class AppController {
     return this.appService.mainRoute();
   }
 
-  @Get("/profile")
+  @Get("/api/profile")
   @UseGuards(AuthGuard())
   @ApiResponse({ status: 200, description: "Request Received" })
   @ApiResponse({ status: 400, description: "Request Failed" })
   getProfile(@Req() req) {
     return req.user;
+  }
+
+  @Delete("/api/profile/:username")
+  @ApiResponse({ status: 200, description: "Request Received" })
+  @ApiResponse({ status: 400, description: "Request Failed" })
+  async delete(@Param("username") username: string) {
+    return await this.profileService.delete(username);
   }
 }
