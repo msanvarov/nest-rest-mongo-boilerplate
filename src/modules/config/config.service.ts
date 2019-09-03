4aae2acb01fc79ac6389c607211764f0dd2f718d
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 16:37:23 -0400

    validation for .env file

--- src/modules/config/config.service.ts
+++ src/modules/config/config.service.ts
@@ -1,18 +1,51 @@
 import * as dotenv from "dotenv";
+import * as joi from "@hapi/joi";
 import * as fs from "fs";
 
+export interface EnvConfig {
+  [key: string]: string;
+}
+
 export class ConfigService {
-  private readonly envConfig: { [key: string]: string };
+  private readonly envConfig: EnvConfig;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath));
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = ConfigService.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private static validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: joi.ObjectSchema = joi.object({
+      APP_ENV: joi
+        .string()
+        .valid(["dev", "prod"])
+        .default("dev"),
+      APP_URL: joi
+        .string()
+        .regex(/:\/\/[0-9a-z-.]+\.[a-z]+\//i)
+        .uri({
+          scheme: [/https?/],
+        }),
+      WEBTOKEN_SECRET_KEY: joi.string().required(),
+      WEBTOKEN_EXPIRATION_TIME: joi.number().default(1800),
+      DB_URL: joi.string().regex(/^mongodb/),
+    });
+
+    const { error, value: validatedEnvConfig } = joi.validate(
+      envConfig,
+      envVarsSchema,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
   }
 
   get(key: string): string {
     return this.envConfig[key];
-  }
-
-  isEnv(env: string) {
-    return this.envConfig.APP_ENV === env;
   }
 }
