db0c93bb30313a1b1f74f06c01260b86056b1a02
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 14:53:32 -0400

    main controller contains more detailed basic methods

--- src/modules/main/app.controller.ts
+++ src/modules/main/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { ApiBearerAuth, ApiResponse } from "@nestjs/swagger";
+import { AuthGuard } from "@nestjs/passport";
 
 @Controller()
+@ApiBearerAuth()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  @UseGuards(AuthGuard())
+  @ApiResponse({ status: 200, description: "Request Received" })
+  @ApiResponse({ status: 400, description: "Request Failed" })
+  getString(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/profile")
+  @UseGuards(AuthGuard())
+  @ApiResponse({ status: 200, description: "Request Received" })
+  @ApiResponse({ status: 400, description: "Request Failed" })
+  getProfile(@Req() req) {
+    return req.user;
   }
 }
