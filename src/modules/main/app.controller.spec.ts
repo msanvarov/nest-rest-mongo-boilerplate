074af731f6124748be8d03daac88d8ce86700aff
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 16:44:35 -0400

    updated test description

--- src/modules/main/app.controller.spec.ts
+++ src/modules/main/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+    it("should return 'Main Route'", () => {
+      expect(appController.getString()).toBe("Main Route");
     });
   });
 });
