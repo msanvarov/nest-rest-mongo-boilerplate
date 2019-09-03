69503f6ec472d453e9c4a8c9864e2cac263637d2
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 14:54:47 -0400

    a gravatar is fetched from email on registration

--- src/modules/profile/profile.service.ts
+++ src/modules/profile/profile.service.ts
@@ -1,35 +1,74 @@
 import * as crypto from "crypto";
+import * as gravatar from "gravatar";
 import { Model } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
-import { Injectable, NotAcceptableException } from "@nestjs/common";
-import { ProfileI, ProfileFillableFields } from "./profile.model";
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotAcceptableException,
+} from "@nestjs/common";
+import { IProfile, ProfileFillableFields } from "./profile.model";
 
 @Injectable()
 export class ProfileService {
   constructor(
-    @InjectModel("Profile") private readonly profileModel: Model<ProfileI>,
+    @InjectModel("Profile") private readonly profileModel: Model<IProfile>,
   ) {}
-  async get(id: number): Promise<ProfileI> {
-    return this.profileModel.findById(id).exec();
+  async get(id: number): Promise<IProfile> {
+    return this.profileModel
+      .findById(id)
+      .then((user: IProfile) =>
+        user
+          ? {
+              _id: user._id,
+              username: user.username,
+              email: user.email,
+              avatar: user.avatar,
+            }
+          : {},
+      )
+      .catch(err => {
+        throw new InternalServerErrorException(err.message);
+      });
   }
 
-  async getByEmail(email: string) {
-    return this.profileModel.findOne({ email }).exec();
+  async getByUsername(username: string) {
+    return this.profileModel
+      .findOne({ username })
+      .then(profile => profile)
+      .catch(err => {
+        throw new InternalServerErrorException(err.message);
+      });
   }
 
-  async getByEmailAndPass(email: string, password: string) {
-    const passHash = crypto.createHmac("sha256", password).digest("hex");
-    return this.profileModel.findOne({ email, password: passHash }).exec();
+  async getByUsernameAndPass(username: string, password: string) {
+    return this.profileModel
+      .findOne({
+        username,
+        password: crypto.createHmac("sha256", password).digest("hex"),
+      })
+      .then(profile => profile)
+      .catch(err => {
+        throw new InternalServerErrorException(err.message);
+      });
   }
 
   async create(payload: ProfileFillableFields) {
-    const user = await this.getByEmail(payload.email);
+    const user = await this.getByUsername(payload.username);
     if (user) {
-      throw new NotAcceptableException("The email already exists in system.");
+      throw new NotAcceptableException(
+        "The username specified already exists in system.",
+      );
     }
     const createdProfile = new this.profileModel({
       ...payload,
       password: crypto.createHmac("sha256", payload.password).digest("hex"),
+      avatar: gravatar.url(payload.email, {
+        protocol: "http",
+        s: "200",
+        r: "pg",
+        d: "404",
+      }),
     });
     return createdProfile.save();
   }
