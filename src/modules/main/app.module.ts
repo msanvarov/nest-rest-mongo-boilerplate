8fbe99d1c1e9511363643e5fcd5dc3d99a4549ac
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 22:14:51 -0400

    winston configurations for logs

--- src/modules/main/app.module.ts
+++ src/modules/main/app.module.ts
@@ -1,3 +1,5 @@
+import * as winston from "winston";
+import * as rotateFile from "winston-daily-rotate-file";
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -6,6 +8,7 @@ import { ConfigModule } from "../config/config.module";
 import { ConfigService } from "../config/config.service";
 import { AuthModule } from "../auth/auth.module";
 import { ProfileModule } from "../profile/profile.module";
+import { WinstonModule } from "../winston/winston.module";
 
 @Module({
   imports: [
@@ -17,6 +20,27 @@ import { ProfileModule } from "../profile/profile.module";
           uri: configService.get("DB_URL"),
           useNewUrlParser: true,
         } as MongooseModuleAsyncOptions),
+    }),
+    WinstonModule.forRoot({
+      level: "info",
+      format: winston.format.json(),
+      defaultMeta: { service: "user-service" },
+      transports: [
+        new winston.transports.File({
+          filename: "logs/error.log",
+          level: "error",
+        }),
+        new winston.transports.Console({
+          format: winston.format.simple(),
+        }),
+        new rotateFile({
+          filename: "logs/application-%DATE%.log",
+          datePattern: "YYYY-MM-DD",
+          zippedArchive: true,
+          maxSize: "20m",
+          maxFiles: "14d",
+        }),
+      ],
     }),
     ConfigModule,
     AuthModule,
