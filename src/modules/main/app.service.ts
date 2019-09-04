f8ae3aa7486a308d46556d4c8dcba5ab145a56a3
msanvarov <salim.anvarov@yahoo.com>
2019-09-03 23:10:39 -0400

    better base route example

--- src/modules/main/app.service.ts
+++ src/modules/main/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "../config/config.service";
 
 @Injectable()
 export class AppService {
+  constructor(private config: ConfigService) {}
+
   mainRoute(): string {
-    return "Main Route";
+    return this.config.get("APP_URL");
   }
 }
