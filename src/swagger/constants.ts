dce60b0382669bf660ec92bec947ffb0b718edb4
msanvarov <salim.anvarov@yahoo.com>
2019-09-24 17:53:28 -0400

    :pencil: documentation coverage

--- src/swagger/constants.ts
+++ src/swagger/constants.ts
@@ -1,7 +1,35 @@
+/**
+ * The endpoint for open api ui
+ * @type {string}
+ */
 export const SWAGGER_API_ROOT = "api/docs";
+/**
+ * The name given to the api
+ * @type {string}
+ */
 export const SWAGGER_API_NAME = "API";
+/**
+ * A short description for api
+ * @type {string}
+ */
 export const SWAGGER_API_DESCRIPTION = "API Description";
+/**
+ * Current version of the api
+ * @type {string}
+ */
 export const SWAGGER_API_CURRENT_VERSION = "1.0";
+/**
+ * The default authentication method
+ * @type {string}
+ */
 export const SWAGGER_API_AUTH_NAME = "Authorization";
+/**
+ * Where the SWAGGER_API_AUTH_NAME will be used in the request
+ * @type {string}
+ */
 export const SWAGGER_API_AUTH_LOCATION = "header";
-export const SWAGGER_API_SCHEMES: ("http" | "https")[] = ["http", "https"];
+/**
+ * Types of api schemes
+ * @type {string[]}
+ */
+export const SWAGGER_API_SCHEMES: Array<"http" | "https"> = ["http", "https"];
