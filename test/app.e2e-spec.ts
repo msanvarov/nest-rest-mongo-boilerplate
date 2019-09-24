0a13949550f56b20182a46633d4e32399a8291b2
msanvarov <salim.anvarov@yahoo.com>
2019-09-24 18:17:49 -0400

    :white_check_mark: updated test suite

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,11 @@
-import { Test, TestingModule } from "@nestjs/testing";
 import * as request from "supertest";
+import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "../src/modules/app/app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { setupSwagger } from "../src/swagger";
 
 describe("AppController (e2e)", () => {
   let app;
+  let bearer;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,40 +13,40 @@ describe("AppController (e2e)", () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupSwagger(app);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it("/ (GET)", () => {
+  it("/ (GET) unauthorized get request", () => {
     return request(app.getHttpServer())
       .get("/")
       .expect(401)
       .expect({ statusCode: 401, error: "Unauthorized" });
   });
 
-  it("/api/auth/login (POST) validate username is alpha", () => {
+  it("/api/auth/login (POST) validate username is alphanumeric", () => {
     return request(app.getHttpServer())
       .post("/api/auth/login")
       .send({
-        username: "123456789",
-        password: "asdasd12321",
+        username: "@#!@@/$%%^)(*+_=",
+        password: "test123456789",
       })
+      .expect(400)
       .expect({
         statusCode: 400,
         error: "Bad Request",
         message: [
           {
             target: {
-              username: "123456789",
-              password: "asdasd12321",
+              username: "@#!@@/$%%^)(*+_=",
+              password: "test123456789",
             },
-            value: "123456789",
+            value: "@#!@@/$%%^)(*+_=",
             property: "username",
             children: [],
             constraints: {
-              isAlpha: "username must contain only letters (a-zA-Z)",
+              isAlphanumeric: "username must contain only letters and numbers",
             },
           },
         ],
@@ -57,19 +57,20 @@ describe("AppController (e2e)", () => {
     return request(app.getHttpServer())
       .post("/api/auth/login")
       .send({
-        username: "asdasdsdads",
-        password: "213",
+        username: "test",
+        password: "<8",
       })
+      .expect(400)
       .expect({
         statusCode: 400,
         error: "Bad Request",
         message: [
           {
             target: {
-              username: "asdasdsdads",
-              password: "213",
+              username: "test",
+              password: "<8",
             },
-            value: "213",
+            value: "<8",
             property: "password",
             children: [],
             constraints: {
@@ -85,14 +86,14 @@ describe("AppController (e2e)", () => {
     return request(app.getHttpServer())
       .post("/api/auth/login")
       .send({
-        username: "msanvarov",
-        password: "SOMEFAKEPASS",
+        username: "BeforeCreatedProfiles",
+        password: "IDontExist?1234",
       })
       .expect(401)
       .expect({
         statusCode: 401,
         error: "Unauthorized",
-        message: "Wrong login combination!",
+        message: "Could not authenticate. Please try again.",
       });
   });
 
@@ -101,8 +102,9 @@ describe("AppController (e2e)", () => {
       .post("/api/auth/register")
       .send({
         username: "test",
+        name: "Test Richard",
         email: "test.test@gmail.com",
-        password: "asdasd12321",
+        password: "test123456789",
       })
       .expect(201);
   });
@@ -112,16 +114,32 @@ describe("AppController (e2e)", () => {
       .post("/api/auth/login")
       .send({
         username: "test",
-        password: "asdasd12321",
+        password: "test123456789",
       })
-      .expect(201);
+      .expect(201)
+      .then(res => (bearer = res.body.token));
   });
 
-  it("/api/auth/register (POST) validate that the same account can't be created twice", () => {
+  it("/ (GET) fetch main route when authorized", () => {
+    return request(app.getHttpServer())
+      .get("/")
+      .set("Authorization", `Bearer ${bearer}`)
+      .expect(200);
+  });
+
+  it("/api/profile (GET) get request user object", () => {
+    return request(app.getHttpServer())
+      .get("/api/profile")
+      .set("Authorization", `Bearer ${bearer}`)
+      .expect(200);
+  });
+
+  it("/api/auth/register (POST) validate that the same account fails to register", () => {
     return request(app.getHttpServer())
       .post("/api/auth/register")
       .send({
         username: "test",
+        name: "test sir",
         email: "test.test@gmail.com",
         password: "123456789",
       })
@@ -129,14 +147,41 @@ describe("AppController (e2e)", () => {
       .expect({
         statusCode: 406,
         error: "Not Acceptable",
-        message: "The username specified already exists in system.",
+        message:
+          "The account with the provided username currently exists. Please choose another one.",
       });
   });
 
-  it("teardown", async () => {
+  it("/api/auth/register (POST) create an account to delete", () => {
+    return request(app.getHttpServer())
+      .post("/api/auth/register")
+      .send({
+        username: "delete",
+        name: "to delete",
+        email: "delete.test@gmail.com",
+        password: "123456789",
+      })
+      .expect(201);
+  });
+
+  it("/api/profile/{username} (DELETE) teardown created account", () => {
+    return request(app.getHttpServer())
+      .delete("/api/profile/delete")
+      .set("Authorization", `Bearer ${bearer}`)
+      .expect(200)
+      .expect({
+        message: "Deleted delete from records",
+      });
+  });
+
+  it("/api/profile/{username} (DELETE) teardown main account", () => {
     return request(app.getHttpServer())
       .delete("/api/profile/test")
-      .expect(200);
+      .set("Authorization", `Bearer ${bearer}`)
+      .expect(200)
+      .expect({
+        message: "Deleted test from records",
+      });
   });
 
   afterAll(async () => {
