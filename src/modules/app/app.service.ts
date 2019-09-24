16ed01ee3528cf22aa0d367b6a266c1e961ed382
msanvarov <salim.anvarov@yahoo.com>
2019-09-24 17:56:08 -0400

    :bulb: docs for methods

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject } from "@nestjs/common";
 import { ConfigService } from "../config/config.service";
+import { Logger } from "winston";
 
+/**
+ * Application Service
+ */
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
+  /**
+   * Constructor
+   * @param {ConfigService} config configuration service
+   * @param {Logger} logger logger service
+   */
+  constructor(
+    private config: ConfigService,
+    @Inject("winston") private readonly logger: Logger,
+  ) {}
 
-  mainRoute(): string {
-    return this.config.get("APP_URL");
+  /**
+   * Main application method
+   * @returns {string} the application url
+   */
+  root(): string {
+    const appURL = this.config.get("APP_URL");
+    this.logger.info("Logging the APP_URL -> " + appURL);
+    return appURL;
   }
 }
