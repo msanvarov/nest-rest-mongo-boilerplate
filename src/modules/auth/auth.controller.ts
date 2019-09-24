b18b8ba8b25ae9989cdcad162f2bcb8dbf3823b6
msanvarov <salim.anvarov@yahoo.com>
2019-09-24 17:56:21 -0400

    :bulb: docs for code

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,30 @@
 import { Controller, Body, Post } from "@nestjs/common";
 import { ApiResponse, ApiUseTags } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
-import { LoginPayload } from "./login.payload";
-import { RegisterPayload } from "./register.payload";
+import { LoginPayload } from "./payload/login.payload";
+import { RegisterPayload } from "./payload/register.payload";
 import { ProfileService } from "../profile/profile.service";
 
+/**
+ * Authentication Controller
+ */
 @Controller("api/auth")
 @ApiUseTags("authentication")
 export class AuthController {
+  /**
+   * Constructor
+   * @param {AuthService} authService authentication service
+   * @param {ProfileService} profileService profile service
+   */
   constructor(
     private readonly authService: AuthService,
     private readonly profileService: ProfileService,
   ) {}
 
+  /**
+   * Authentication route to login
+   * @param {LoginPayload} payload the login dto
+   */
   @Post("login")
   @ApiResponse({ status: 201, description: "Login Completed" })
   @ApiResponse({ status: 400, description: "Bad Request" })
@@ -22,6 +34,10 @@ export class AuthController {
     return await this.authService.createToken(user);
   }
 
+  /**
+   * Authentication route to register
+   * @param {RegisterPayload} payload the registration dto
+   */
   @Post("register")
   @ApiResponse({ status: 201, description: "Registration Completed" })
   @ApiResponse({ status: 400, description: "Bad Request" })
