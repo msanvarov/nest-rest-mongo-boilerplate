68c498151478355ac24243d34d76b608ce1d085b
Sal <salim.anvarov@yahoo.com>
2019-10-29 09:52:26 -0400

    📝 clearer comments on docs

--- src/modules/winston/winston.constants.ts
+++ src/modules/winston/winston.constants.ts
@@ -1,10 +1,10 @@
 /**
- * Token for the type of configuration to be used when declaring in the app module
+ * Token for the type of configuration to be used when declaring in app module
  * @type{string}
  */
 export const WINSTON_MODULE_OPTIONS = "WinstonModuleOptions";
 /**
- * An identifier to use for injecting the winston provider
+ * An identifier for when injecting the winston provider
  * @type{string}
  */
 export const WINSTON_MODULE_PROVIDER = "winston";
